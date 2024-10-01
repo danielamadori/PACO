4563083a5dc10776023ec228c94b02c73d21897d
Daniel Amadori <daniel.amadori@outlook.it>
2024-10-02 00:15:48 +0200

    Starting with pareto_binary_search

--- dash_py/optimizer/pareto_optimizer.py
+++ dash_py/optimizer/pareto_optimizer.py
@@ -1,41 +1,53 @@
 import numpy as np
 import random
-from solver.pareto import get_non_dominated_impacts
+
+from solver.found_strategy import compare_bound
+from solver.pareto import get_non_dominated_impacts, get_dominated_impacts
 from solver.solver import paco
 from utils.env import IMPACTS_NAMES
 
 
-
-
-def pareto_optimal_impacts(bpmn: dict, decimal_number: int = 0):
-	bound = np.zeros(len(bpmn[IMPACTS_NAMES]), dtype=np.float64)
+def pareto_optimal_impacts(bpmn: dict, bound: np.ndarray = None, decimal_number: int = 0):
 	parse_tree = None
 	execution_tree = None
+	if bound is None:
+		bound = np.zeros(len(bpmn[IMPACTS_NAMES]))
 
-	found = False
-	pareto_frontier_impacts = []
+	text_result, parse_tree, execution_tree, found, min_frontier_expected_impacts, choices, name_svg = paco(bpmn, bound, parse_tree, execution_tree)
+	max_frontier_expected_impacts = [execution_tree.root.cei_bottom_up]
 
 	i = 0
-	while not found:
-		text_result, parse_tree, execution_tree, found, expected_impacts, choices, name_svg = paco(bpmn, bound, parse_tree, execution_tree)
+	while True:
+		min_bound = np.minimum.reduce(min_frontier_expected_impacts)
+		max_bound = np.maximum.reduce(max_frontier_expected_impacts)
+		mean_bound = (min_bound + max_bound) / 2
 
-		pareto_frontier_impacts.extend([np.round(ei, decimal_number) for ei in expected_impacts])
-		pareto_frontier_impacts = get_non_dominated_impacts(pareto_frontier_impacts)
+		#min_bound = min_frontier_expected_impacts.pop(random.randint(0, len(min_frontier_expected_impacts)-1))
 
-		if not found:
-			bound = pareto_frontier_impacts.pop(random.randint(0, len(pareto_frontier_impacts)-1))
+		text_result, parse_tree, execution_tree, found, min_expected_impacts, choices, name_svg = paco(bpmn, mean_bound, parse_tree, execution_tree)
+
+		'''
+		max_frontier_expected_impacts.extend([np.round(ei, decimal_number) for ei in max_expected_impacts])
+		max_frontier_expected_impacts = get_dominated_impacts(max_frontier_expected_impacts)
+		'''
+		min_frontier_expected_impacts.extend([np.round(ei, decimal_number) for ei in min_expected_impacts])
+		min_frontier_expected_impacts = get_non_dominated_impacts(min_frontier_expected_impacts)
 
 		s = ""
-		for j in range(len(pareto_frontier_impacts)):
-			s += f"Impacts {j}:\t{pareto_frontier_impacts[j]}\n"
-		print(f"Attempt {i}:\t{bpmn[IMPACTS_NAMES]}\nSelected:\t{bound}\n" + s)
+		for j in range(len(min_frontier_expected_impacts)):
+			s += f"Impacts {j}:\t{min_frontier_expected_impacts[j]}\n"
+		print(f"Attempt {i}:\t{bpmn[IMPACTS_NAMES]}\nSelected:\t{mean_bound}\n" + s)
 
 		i += 1
 
-	return bound, pareto_frontier_impacts, parse_tree, execution_tree, choices
+		if found: #and np.all(compare_bound(min_frontier_expected_impacts[0], bound) <= 0)
+			break
+
+	return bound, min_frontier_expected_impacts, parse_tree, execution_tree, choices
 
 
 
+'''
 from multiprocessing import Process, Queue
 
 # Worker function to execute paco and send results via a queue
@@ -106,3 +118,4 @@ def pareto_optimal_impacts_parallel(bpmn: dict, decimal_number: int = 0, max_wor
 		i += 1
 
 	return bound, pareto_frontier_impacts, parse_tree, execution_tree, choices
+'''
--- dash_py/solver/pareto.py
+++ dash_py/solver/pareto.py
@@ -17,3 +17,20 @@ def get_non_dominated_impacts(frontier):
 				non_dominated_impacts.append(arr)
 
 	return non_dominated_impacts
+
+def get_dominated_impacts(frontier):
+	dominated_impacts = []
+
+	for i, arr in enumerate(frontier):
+		dominated = False
+		for j, other_arr in enumerate(frontier):
+			if (i != j and  # arr is dominated by other_arr
+					np.all(arr >= other_arr) and np.any(arr > other_arr)):
+				dominated = True
+				break
+		if dominated:
+			# Check for duplicates
+			if not any(np.array_equal(arr, x) for x in dominated_impacts):
+				dominated_impacts.append(arr)
+
+	return dominated_impacts
