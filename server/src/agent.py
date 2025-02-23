2746d75993c712c34f26ca0afa3b049022cfb41f
emanuele_chini <emanuele.chini@studenti.univr.it>
2025-02-23 14:38:22 +0100

    modificato print diagram tolto pydot perché andava in ciclo infinito quiondi ora restituisce il diagramma in formato str e non pydot. Tolti token perché modo non corretto di gestire l'autenticazione Fatto un po' di pulizia in agent e requirements e tolte le immagini

--- server/src/agent.py
+++ server/src/agent.py
@@ -1,78 +1,6 @@
 from langchain_openai import ChatOpenAI
-from langchain.agents import tool
 from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
 from ai.prompt import examples_bpmn, define_role
-from paco.parser.bpmn_parser import SESE_PARSER
-# https://github.com/RenaudLN/dash_socketio/tree/main
-
-# docker build -t paco .
-# docker run -d -p 8050:8050 -it --name paco paco 
-
-@tool
-def check_correct_process(expression: str) -> bool:
-    """
-    Checks if the given BPMN process expression is correct according to the SESE diagram grammar.
-
-    Args:
-        expression (str): The BPMN process expression to be checked.
-
-    Returns:
-        bool: True if the expression is correct and can be parsed by SESE_PARSER, False otherwise.
-
-    Raises:
-        Exception: If the expression cannot be parsed, an exception is caught and False is returned.
-    """
-    try:
-        SESE_PARSER.parse(expression)
-        return True
-    except Exception as e:
-        return False
-
-
-
-# url = '157.27.193.108'
-# memory = MemorySaver()
-# model: ChatOpenAI = ChatOpenAI(
-#     base_url=f"http://{url}:1234/v1",
-#     temperature=0.7,
-#     api_key="lm-studio",
-# )
-
-# example_prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("human", "{input}"),
-#         ("ai", "{answer}"),
-#     ]
-# )
-
-# few_shot_prompt = FewShotChatMessagePromptTemplate(
-#     example_prompt=example_prompt,
-#     examples=examples_bpmn,
-# )
-
-# final_prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", define_role),
-#         few_shot_prompt,
-#         ("human", "{input}"),
-#     ]
-# )
-# print(few_shot_prompt.invoke({}).to_messages())
-# chain = final_prompt | model
-# config = {"configurable": {"thread_id": "abc123"}}
-# chain.invoke(
-#     {"input": "I have to complete the writing task before having a nature between talking with the publisher or to print the page written. Then, i choose between going to the park or continue writing"})
-# for chunk in chain.stream(
-    
-#     {"input": [HumanMessage(content="I have to complete the writing task before having a nature between talking with the publisher or to print the page written. Then, i choose between going to the park or continue writing")]}, config
-# ):
-#     print(chunk.content, end='', flush=True)
-# while True:
-#     user_input = input("You: ")
-#     for chunk in chain.stream(
-#         {"input": user_input}, config
-#     ):
-#         print(chunk.content, end='', flush=True)
 
 def define_agent(
         url:str = '157.27.193.108', verbose = False,
@@ -129,24 +57,3 @@ def define_agent(
         except Exception as e:
             print(e)
     return chain, config
-
-# chain, config = define_agent(verbose=True)
-# chain.invoke(
-#     {"input": "I have to complete the writing task before having a nature between talking with the publisher or to print the page written. Then, i choose between going to the park or continue writing"})
-
-# tools = [get_word_length]
-# agent_executor = create_react_agent(model, tools, checkpointer=memory)
-
-# # Use the agent
-
-# for chunk in agent_executor.stream(
-#     {"messages": [HumanMessage(content="hi im bob! and i live in sf")]}, config
-# ):
-#     print(chunk)
-#     print("----")
-
-# for chunk in agent_executor.stream(
-#     {"messages": [HumanMessage(content="whats the weather where I live?")]}, config
-# ):
-#     print(chunk)
-#     print("----")
--- server/src/server.py
+++ server/src/server.py
@@ -10,7 +10,7 @@ from paco.explainer.build_explained_strategy import build_explained_strategy
 from paco.parser.create import create
 from paco.searcher.search import search
 from paco.parser.bpmn_parser import SESE_PARSER
-from utils.print_sese_diagram import print_sese_diagram
+from paco.parser.print_sese_diagram import print_sese_diagram
 from utils.env import ALGORITHMS, DELAYS, DURATIONS, IMPACTS, IMPACTS_NAMES, LOOP_PROB, NAMES, RESOLUTION, PROBABILITIES
 from paco.solver import paco
 from utils.check_syntax import check_algo_is_usable, checkCorrectSyntax, check_input
@@ -71,7 +71,6 @@ class BPMNPrinting(BaseModel):
         graph_options (Dict, optional): Graph rendering options
     """    
     bpmn: BPMNDefinition
-    resolution_bpmn: Optional[int] = RESOLUTION
     graph_options: Optional[Dict] = {}
 
 class StrategyFounderAlgo(BaseModel):
@@ -100,7 +99,7 @@ async def get():
     return f"welcome to PACO server"
 
 @app.get("/check_correct_process_expression")
-async def check_correct_process_expression(expression: str, token:str = None) -> bool:
+async def check_correct_process_expression(expression: str) -> bool:
     """
     Checks if the given BPMN process expression is correct according to the SESE diagram grammar.
 
@@ -114,8 +113,6 @@ async def check_correct_process_expression(expression: str, token:str = None) ->
         Exception: If the expression cannot be parsed, an exception is caught and False is returned.
         403: If the token is not provided, an exception not authorized.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     try:
         SESE_PARSER.parse(expression)
         return True
@@ -125,7 +122,7 @@ async def check_correct_process_expression(expression: str, token:str = None) ->
 
 
 @app.get("/check_input")
-async def check_input_bpmn(bpmn: BPMNDefinition, bound: list[float], token:str = None) -> tuple[str, List[int]]:
+async def check_input_bpmn(bpmn: BPMNDefinition, bound: list[float]) -> tuple[str, List[int]]:
     """
     Checks the input of the given BPMN process and bound.
 
@@ -140,8 +137,6 @@ async def check_input_bpmn(bpmn: BPMNDefinition, bound: list[float], token:str =
         400: If the input is not valid, an exception is raised.
         403: If the token is not provided, an exception not authorized.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(bpmn, BPMNDefinition) or not isinstance(bound, list):
         return HTTPException(status_code=400, detail="Invalid input")
     try:
@@ -151,7 +146,7 @@ async def check_input_bpmn(bpmn: BPMNDefinition, bound: list[float], token:str =
         return HTTPException(status_code=500, detail=str(e))
 
 @app.get("/check_algo_usable")
-async def check_algo_usable(sfa :StrategyFounderAlgo, token:str = None) -> bool:
+async def check_algo_usable(sfa :StrategyFounderAlgo) -> bool:
     """
     Checks if the given algorithm is usable for the given BPMN process.
 
@@ -166,66 +161,14 @@ async def check_algo_usable(sfa :StrategyFounderAlgo, token:str = None) -> bool:
         400: If the algorithm is not valid, an exception is raised.
         403: If the token is not provided, an exception not authorized.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(sfa.bpmn, BPMNDefinition) or not isinstance(sfa.algo, str):
         return HTTPException(status_code=400, detail="Invalid input")
     if not check_algo_is_usable(sfa.bpmn.expression, sfa.algo):
         return False
     return True
 
-
-@app.get("/print_sese_diagram")
-async def print_bpmn(request: BPMNPrinting, token: str = None):
-    """
-    Generates and returns a BPMN diagram image
-    Args:
-        request (BPMNPrinting): BPMN printing configuration
-        token (str): Authorization token
-    Returns:
-        FileResponse: PNG image of the BPMN diagram
-    Raises:
-        HTTPException: 400 for invalid input, 500 for server errors, 403 for unauthorized access
-    """
-    bpmn = request.bpmn
-    if token == None:
-        raise HTTPException(status_code=403, detail="Not authorized")
-    if not isinstance(bpmn, BPMNDefinition):
-        raise HTTPException(status_code=400, detail="Invalid input")
-    if not checkCorrectSyntax(dict(bpmn)):
-        raise HTTPException(status_code=400, detail="Invalid BPMN syntax")   
-    try:
-        
-        try:
-            img = print_sese_diagram(expression=bpmn.expression,
-                h=bpmn.h, 
-                probabilities=bpmn.probabilities, 
-                impacts=bpmn.impacts, 
-                loop_thresholds=bpmn.loop_thresholds, 
-                graph_options=request.graph_options, 
-                durations=bpmn.durations, 
-                names=bpmn.names, 
-                delays=bpmn.delays, 
-                impacts_names=bpmn.impacts_names, 
-                resolution_bpmn=request.resolution_bpmn, 
-                loop_round=bpmn.loop_round, 
-                loops_prob=bpmn.loops_prob
-            )
-            print('GRAPH' , img)
-        except Exception as e:
-            print('--------------------------------')
-            print(e)
-            print('--------------------------------')
-            raise HTTPException(status_code=505, detail=str(e))
-        return {
-            "graph": img
-        }
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
 @app.get("/check_syntax")
-async def check_syntax(bpmn: BPMNDefinition, token:str = None) -> bool:
+async def check_syntax(bpmn: BPMNDefinition) -> bool:
     """
     Checks the syntax of the given BPMN process.
 
@@ -239,8 +182,6 @@ async def check_syntax(bpmn: BPMNDefinition, token:str = None) -> bool:
         400: If the input is not valid, an exception is raised.
         403: If the token is not provided, an exception not authorized.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(bpmn, BPMNDefinition):
         return HTTPException(status_code=400, detail="Invalid input")
     return checkCorrectSyntax(dict(bpmn))
@@ -265,7 +206,7 @@ async def get_algorithms(token:str = None) -> dict:
 
 
 @app.get("/calc_strategy_general")
-async def calc_strategy_and_explainer(request: StrategyFounderAlgo, token: str = None) -> dict:
+async def calc_strategy_and_explainer(request: StrategyFounderAlgo, ) -> dict:
     """
     Calculates strategy using PACO algorithm
     Args:
@@ -275,8 +216,6 @@ async def calc_strategy_and_explainer(request: StrategyFounderAlgo, token: str =
     Raises:
         HTTPException: 400 for invalid input, 500 for server errors
     """
-    if token == None:
-        raise HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(request.bpmn, BPMNDefinition):
         raise HTTPException(status_code=400, detail="Invalid input")
     bpmn = dict(request.bpmn)
@@ -306,12 +245,12 @@ async def calc_strategy_and_explainer(request: StrategyFounderAlgo, token: str =
         raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/excecution_tree")
-async def get_excecution_tree(bpmn:BPMNDefinition = None, token:str = None) -> tuple:
+async def get_excecution_tree(bpmn:BPMNDefinition = None) -> tuple:
     """
     Get the execution tree.
 
     Args:
-        token (str): The token of the agent.
+        
         bpmn (BPMNDefinition): The BPMN process.
 
     Returns:
@@ -322,8 +261,6 @@ async def get_excecution_tree(bpmn:BPMNDefinition = None, token:str = None) -> t
         400: If the input is not valid, an exception is raised.
         500: If an error occurs, an exception is raised.
     """
-    if token == None:
-        raise HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(bpmn, BPMNDefinition) or bpmn == None:
         raise HTTPException(status_code=400, detail="Invalid input")
     try:        
@@ -338,7 +275,7 @@ async def get_excecution_tree(bpmn:BPMNDefinition = None, token:str = None) -> t
 @app.get("/search_only_strategy")
 async def search_strategy(
     impacts_names: list[str], execution_tree, #: ExecutionTreeModel, 
-    bound: list[float], search_only: bool = False, token:str = None
+    bound: list[float], search_only: bool = False
     ) -> tuple:
     """
     calcolate ONLY the strategy.
@@ -347,7 +284,7 @@ async def search_strategy(
         execution_tree (ExecutionTreeModel): The execution tree.
         bound (np.ndarray): The bound.
         search_only (bool): If True, only search for a solution without building the strategy.
-        token (str): The token of the agent.
+        
 
     Returns:
         tuple: The expected impacts, possible min solution, solutions, and strategy.
@@ -357,8 +294,6 @@ async def search_strategy(
         400: If the input is not valid, an exception is raised.
         500: If an error occurs, an exception is raised.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(impacts_names, list[str]) or not isinstance(bound, np.ndarray) or not isinstance(search_only, bool):
         return HTTPException(status_code=400, detail="Invalid input")
     try:
@@ -412,7 +347,7 @@ async def search_strategy(
 
 @app.get("/explainer")
 async def get_explainer(
-    parse_tree, strategy, type_explainer: str, impacts_names: list[str], name_svg: str = '', token:str = None
+    parse_tree, strategy, type_explainer: str, impacts_names: list[str], name_svg: str = ''
     ) -> tuple:
     """
     Get the explainer.
@@ -423,7 +358,7 @@ async def get_explainer(
         type_explainer (str): The type of explainer.
         impacts_names (list[str]): The impacts names.
         name_svg (str): The name of the svg.
-        token (str): The token of the agent.
+        
 
     Returns:
         tuple: The strategy tree, expected impacts, strategy expected time, choices, and name svg.
@@ -433,8 +368,6 @@ async def get_explainer(
         400: If the input is not valid, an exception is raised.
         500: If an error occurs, an exception is raised.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(strategy, list) or not isinstance(type_explainer, str) or not isinstance(impacts_names, list[str]) or not isinstance(name_svg, str):
         return HTTPException(status_code=400, detail="Invalid input")
     try:
@@ -457,7 +390,6 @@ async def get_explainer(
 async def get_agent(
     url: str, api_key: str, 
     model: str, temperature: float,
-    token: str = None,
     ):
     """
     Define the agent with the given parameters.
@@ -467,13 +399,11 @@ async def get_agent(
         api_key (str): The api key of the agent.
         model (str): The model of the agent.
         temperature (float): The temperature of the agent.
-        token (str): The token of the agent.
+        
 
     Returns:
        tuple: The agent defined and the configuration.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(url, str) or not isinstance(api_key, str) or not isinstance(model, str) or not isinstance(temperature, float):
         return HTTPException(status_code=400, detail="Invalid input")
     try:
@@ -489,13 +419,13 @@ async def get_agent(
 
 
 @app.get("/invoke-agent")
-async def invoke_agent(prompt:str, llm, history:list[tuple],  token:str = None) ->  tuple[str, list[tuple]]:
+async def invoke_agent(prompt:str, llm, history:list[tuple]) ->  tuple[str, list[tuple]]:
     """
     Invoke the agent with the given prompt.
 
     Args:
         prompt (str): The prompt to be used.
-        token (str): The token of the agent.
+        
         llm: The agent defined.
         history: The history of the agent.
 
@@ -503,8 +433,6 @@ async def invoke_agent(prompt:str, llm, history:list[tuple],  token:str = None) 
         str: The response from the agent.
         list[tuple]: The history of the agent.
     """
-    if token == None:
-        return HTTPException(status_code=403, detail="Not authorized")
     if not isinstance(prompt, str):
         return HTTPException(status_code=400, detail="Invalid input")
     try:
@@ -547,7 +475,7 @@ async def login(request: LoginRequest):
 async def set_bpmn(
     bpmn_lark: dict, impacts_table, 
     durations, task: str,
-    loops, probabilities, delays, token: str = None):
+    loops, probabilities, delays, ):
     try:
         bpmn_lark[IMPACTS] = cs.extract_impacts_dict(bpmn_lark[IMPACTS_NAMES], impacts_table) 
         # print(bpmn_lark[IMPACTS])
@@ -570,21 +498,45 @@ async def set_bpmn(
             raise HTTPException(status_code=400, detail="Invalid BPMN syntax")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-#######################################################
-############ PUT ######################################
-#######################################################
 
-# @app.put("/update")
-# async def put():
-#     pass
+@app.post("/create_sese_diagram")
+async def print_bpmn(request: BPMNPrinting):
+    """
+    Generates and returns a BPMN diagram str that then can be converted to a pydot object.
+    Args:
+        request (BPMNPrinting): BPMN printing configuration
+    Returns:
+        FileResponse: PNG image of the BPMN diagram
+    Raises:
+        HTTPException: 400 for invalid input, 500 for server errors, 403 for unauthorized access
+    """
+    bpmn = request.bpmn
+    if not isinstance(request, BPMNPrinting):
+        raise HTTPException(status_code=400, detail="Invalid input")
+    if not checkCorrectSyntax(dict(bpmn)):
+        raise HTTPException(status_code=400, detail="Invalid BPMN syntax")   
+    try:        
+        print('BPMN', dict(bpmn))
+        print(request)
+        img = print_sese_diagram(expression=bpmn.expression,
+            h=bpmn.h, 
+            probabilities=bpmn.probabilities, 
+            impacts=bpmn.impacts, 
+            loop_thresholds=bpmn.loop_thresholds, 
+            graph_options=request.graph_options, 
+            durations=bpmn.durations, 
+            names=bpmn.names, 
+            delays=bpmn.delays, 
+            impacts_names=bpmn.impacts_names, 
+            loop_round=bpmn.loop_round, 
+            loop_probability=bpmn.loop_probability
+        )
+        return {
+            "graph": img
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-#######################################################
-############ DELETE ###################################
-#######################################################
-
-# @app.delete("/delete")
-# async def delete():
-#     pass
 
 #######################################################
 #### MAIN #############################################
@@ -593,107 +545,5 @@ async def set_bpmn(
 if __name__ == '__main__':   
     uvicorn.run(
         app, 
-        host="0.0.0.0", port=8000,
-        # ssl_keyfile=os.getenv("SSL_KEY"),      # Optional: SSL key
-        # ssl_certfile=os.getenv("SSL_CERT")   # Optional: SSL certificate
+        host="0.0.0.0", port=8000
     )
-
-
-
-# @app.get("/print_sese_diagram")
-# async def print_bpmn(request: BPMNPrinting, token: str = None) -> FileResponse:
-#     """
-#     Generates and returns a BPMN diagram image
-#     Args:
-#         request (BPMNPrinting): BPMN printing configuration
-#         token (str): Authorization token
-#     Returns:
-#         FileResponse: PNG image of the BPMN diagram
-#     Raises:
-#         HTTPException: 400 for invalid input, 500 for server errors, 403 for unauthorized access
-#     """
-#     bpmn = request.bpmn
-#     if token == None:
-#         raise HTTPException(status_code=403, detail="Not authorized")
-#     if not isinstance(bpmn, BPMNDefinition):
-#         raise HTTPException(status_code=400, detail="Invalid input")
-#     if not checkCorrectSyntax(dict(bpmn)):
-#         raise HTTPException(status_code=400, detail="Invalid BPMN syntax")   
-#     try:
-        
-#         try:
-#             img = print_sese_diagram(expression=bpmn.expression,
-#                 h=bpmn.h, 
-#                 probabilities=bpmn.probabilities, 
-#                 impacts=bpmn.impacts, 
-#                 loop_thresholds=bpmn.loop_thresholds, 
-#                 graph_options=request.graph_options, 
-#                 durations=bpmn.durations, 
-#                 names=bpmn.names, 
-#                 delays=bpmn.delays, 
-#                 impacts_names=bpmn.impacts_names, 
-#                 resolution_bpmn=request.resolution_bpmn, 
-#                 loop_round=bpmn.loop_round, 
-#                 loops_prob=bpmn.loops_prob
-#             )
-#             print('GRAPH' , img)
-#         except Exception as e:
-#             print('--------------------------------')
-#             print(e)
-#             print('--------------------------------')
-#             raise HTTPException(status_code=505, detail=str(e))
-#         return FileResponse(f'src/{PATH_IMAGE_BPMN_LARK}', media_type='image/png', filename='bpmn.png')
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-# @app.get("/bpmn_img")
-# async def get_bpmn_img(name:str, token: str = None) -> FileResponse:
-#     """
-#     Returns the BPMN diagram image with the given name.
-#     Args:
-#         name (str): Name of the BPMN diagram image
-#         token (str): Authorization token
-#     Returns:
-#         FileResponse: PNG image of the BPMN diagram
-#     Raises:
-#         HTTPException: 403 for unauthorized access
-#     """
-#     if token == None:
-#         return HTTPException(status_code=403, detail="Not authorized")
-#     return FileResponse(f'assets/bpmnSvg/{name}.png', media_type='image/png', filename=f'{name}.png')
-# @app.get("/bpmn_example_img")
-# async def get_bpmn_img(name:str, type:str = 'png', token: str = None) -> FileResponse:
-#     """
-#     Returns the BPMN diagram image with the given name.
-#     Args:
-#         name (str): Name of the BPMN diagram image
-#         token (str): Authorization token
-#     Returns:
-#         FileResponse: PNG image of the BPMN diagram
-#     Raises:
-#         HTTPException: 403 for unauthorized access
-#     """
-#     if token == None:
-#         return HTTPException(status_code=403, detail="Not authorized")
-#     if type == 'svg':
-#         return FileResponse(f'assets/examples/{name}.svg', media_type='image/svg+xml', filename=f'{name}.svg')
-#     return FileResponse(f'assets/examples/{name}.png', media_type='image/png', filename=f'{name}.png')
-# @app.get("/bpmn_grammar")
-# async def get_grammar(
-#     token:str = None
-#     ) -> str:
-#     """
-#     Returns the SESE diagram grammar.
-
-#     Returns:
-#         str: The SESE diagram grammar.
-
-#     Raises:
-#         403: If the token is not provided, an exception not authorized.
-#     """
-#     if token == None:
-#         raise HTTPException(status_code=403, detail="Not authorized")
-#     try:
-#         return sese_diagram_grammar
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
