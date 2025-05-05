ae0c06f75dbb2ce604b95b2c932e8bd5f034c9e7
Daniel Amadori <daniel.amadori@outlook.it>
2025-05-05 17:28:01 +0200

    Fix

--- gui/src/view/home/sidebar/strategy_tab/bdd.py
+++ gui/src/view/home/sidebar/strategy_tab/bdd.py
@@ -1,35 +1,6 @@
-from dash import html, dcc
+from dash import html
 import dash_bootstrap_components as dbc
-
-from view.visualizer.RenderSVG import RenderSvg
-
-def get_bdds_visualizer(choice, type_strategy, svg):
-	if type_strategy == "FORCED_DECISION":
-		return html.Div(
-			html.Iframe(
-				src=svg,
-				style={"width": "100%", "height": "100%", "border": "none"}
-			),
-			style={
-					"height": "180px",
-					"border": "1px solid #eee"
-				}
-		)
-
-	visualizer = RenderSvg(
-		type="bdd",
-		index=choice,
-		zoom_bar_placement="right",
-		zoom_bar_height=250,
-		max_height=350
-	)
-
-	return html.Div([
-		dcc.Store(id={"type": "bdd-store", "index": choice}, data=svg),
-		visualizer.get_visualizer()],
-		style={"height": "auto", "maxHeight": "350px", "minHeight": "150px", "width": "100%", "overflow": "auto"}
-	)
-
+from view.home.sidebar.strategy_tab.bdd_visualizer import get_bdds_visualizer
 
 
 def get_bdds(bdds: dict):
